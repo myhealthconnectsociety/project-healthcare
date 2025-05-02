25e29fcfcf244ed999d0922395fb17868ea0297b
Pikender Sharma <avid.piks@gmail.com>
2025-05-02 23:11:40 +0530

    used Settings pydantic_settings to load API_SECRET
    then use the settings to get api_secret to compare in middleware

--- xcov19/app/middleware.py
+++ xcov19/app/middleware.py
@@ -2,7 +2,7 @@ from typing import Callable, Awaitable
 
 from blacksheep import Application, Request, Response, bad_request
 
-from xcov19.app.settings import FromOriginMatchHeader
+from xcov19.app.settings import FromOriginMatchHeader, load_settings
 
 
 def configure_middleware(app: Application, *middlewares):
@@ -12,12 +12,13 @@ def configure_middleware(app: Application, *middlewares):
 async def origin_header_middleware(
     request: Request, handler: Callable[[Request], Awaitable[Response]]
 ) -> Response:
+    api_secret = load_settings().api_secret.encode()
     if not FromOriginMatchHeader.name:
         raise ValueError("FromOriginMatchHeader name is not set.")
     if request.path.startswith("/docs") or request.path.startswith("/openapi"):
         return await handler(request)
     match request.headers.get(FromOriginMatchHeader.name.encode()):
-        case (b"secret",):
+        case x if api_secret in x:
             return await handler(request)
         case _:
             return bad_request("Invalid origin match header value provided.")
